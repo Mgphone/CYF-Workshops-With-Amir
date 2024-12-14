6da65b6991f9bf6384569c7eac1b824d6092224a
mgphone <pmn.blazer@gmail.com>
2024-12-14 14:13:16 +0000

    fix for decrement

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -1,7 +1,7 @@
 //increments the number in a node's text
-function increment(node) {
+function decrement(node) {
   let current = node.textContent;
-  node.textContent = Number(current) + 1;
+  node.textContent = Number(current) - 1;
 }
 
 export function App() {
@@ -10,21 +10,23 @@ export function App() {
   const header = document.createElement("header");
   header.innerHTML = `
         <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
+        <p>A simple counter. Press decrement to decrease the count by one.
+        
+        </p>
     `;
   body.appendChild(header);
 
   const main = document.createElement("main");
   main.innerHTML = `
         <p id="counter" data-testid="counter">0</p>
-        <button id="increment">Increment</button>
+        <button id="decrement">Decrement</button>
     `;
   body.appendChild(main);
 
-  const button = body.querySelector("#increment");
+  const button = body.querySelector("#decrement");
   const counter = body.querySelector("#counter");
   button.addEventListener("click", () => {
-    increment(counter);
+    decrement(counter);
   });
 
   return body;
